8bae75cb4029d9a49fa269e542e2aa72603676f9
Yonatan Karp-Rudin <yonatan.karp@sumup.com>
2022-10-02 20:11:34 +0200

    Remove redundant controller

--- ai-guess-game/src/main/kotlin/com/yonatankarp/ai.guess.game/RiddleController.kt
+++ ai-guess-game/src/main/kotlin/com/yonatankarp/ai.guess.game/RiddleController.kt
@@ -1,19 +1,57 @@
 package com.yonatankarp.ai.guess.game
 
+import org.slf4j.LoggerFactory
 import org.springframework.http.MediaType
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseBody
-import org.springframework.web.bind.annotation.RestController
+import kotlin.random.Random
 
-@RestController
-class RiddleController(private val service: RiddleService) {
+@Controller
+class RiddleController(
+    val riddleService: RiddleService,
+    val riddleManager: RiddleManager
+) {
+
+    companion object {
+        private val log = LoggerFactory.getLogger(RiddleController::class.java)
+
+        private const val MAX_NUMBER_OF_RIDDLES = 3
+    }
+
+    @RequestMapping(value = ["/", "index", "index.html"])
+    fun index(model: Model): String {
+        val riddleIndex = Random.nextInt(from = 1, until = MAX_NUMBER_OF_RIDDLES + 1)
+        log.info("Reading riddle id: $riddleIndex")
+        model.addAttribute("guess", Guess("Enter a word..."))
+        model.addAttribute("riddle", riddleManager.getRiddle(riddleIndex))
+        return "index"
+    }
+
     @GetMapping(value = ["/{id}"], produces = [MediaType.IMAGE_PNG_VALUE])
     @ResponseBody
-    fun getImage(@PathVariable id: Int) = service.getImage(id)
+    fun getImage(@PathVariable id: Int) = riddleService.getImage(id)
 
-    @PostMapping(value = ["/{id}/guess"], produces = [MediaType.APPLICATION_JSON_VALUE])
-    fun guess(@PathVariable id: Int, @RequestBody guess: Guess) = service.handleGuess(id, guess)
+    @RequestMapping(value = ["/{id}"])
+    fun getRiddle(@PathVariable id: Int, model: Model): String {
+        model.addAttribute("riddle", riddleManager.getRiddle(id))
+        return "index"
+    }
+
+    @PostMapping("/{id}/guess")
+    fun submitGuess(@PathVariable id: Int, @ModelAttribute guess: Guess, model: Model): String {
+        log.info("Guess for id $id is ${guess.phrase}")
+        riddleService.handleGuess(id, guess)
+            .let {
+                model.addAttribute("riddle", riddleManager.getRiddle(id))
+                model.addAttribute("guess", guess)
+                model.addAttribute("results", it)
+            }
+        return "index"
+    }
 }
